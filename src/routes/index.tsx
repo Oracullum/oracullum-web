ac6d70fc2b47ce019562316a6954ba1b12a27fa9
Bruno Futema <bruno.futema@outlook.com>
2020-11-06 23:15:38 -0300

    feat: :sparkles: Add basic structure for SignUp page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from '../pages/Landing';
 import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Landing} />
       <Route path="/login" component={SignIn} />
+      <Route path="/register" component={SignUp} />
     </Switch>
   </BrowserRouter>
 );
