9b8b73e9378ee6e2a7760994f295e995a40e292d
Bruno Futema <bruno.futema@outlook.com>
2020-11-06 23:14:46 -0300

    feat: Add basic structure for SignIn page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from '../pages/Landing';
+import SignIn from '../pages/SignIn';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Landing} />
+      <Route path="/login" component={SignIn} />
     </Switch>
   </BrowserRouter>
 );
