1c7a980bba89959f166bdf4ff21a4a3ff03d1d2f
Bruno Futema <bruno.futema@outlook.com>
2020-11-07 03:27:26 -0300

    feat: :sparkles: Add Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,82 @@
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+  useContext,
+} from 'react';
+import { IconBaseProps } from 'react-icons';
+import { FiInfo } from 'react-icons/fi';
+import { useField } from '@unform/core';
+import { ThemeContext } from 'styled-components';
+import Ink from 'react-ink';
+
+import { Container, IconContainer, Error } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+  iconClick?: () => void;
+}
+
+const Input: React.FC<InputProps> = ({
+  name,
+  icon: Icon,
+  iconClick,
+  ...rest
+}) => {
+  const { colors } = useContext(ThemeContext);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
+      {Icon && (
+        <IconContainer onClick={iconClick}>
+          <Icon size={20} />
+          <Ink />
+        </IconContainer>
+      )}
+
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+
+      {error && (
+        <Error title={error}>
+          <FiInfo color={colors.red} size={20} />
+        </Error>
+      )}
+    </Container>
+  );
+};
+
+export default Input;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,104 @@
+import styled, { css } from 'styled-components';
+
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: ${props => props.theme.colors.white};
+  border-radius: 12px;
+  border: 3px solid ${props => props.theme.colors.primaryLighter};
+  padding: 8px;
+  width: 100%;
+  color: ${props => props.theme.colors.grayLight};
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 46px;
+
+  & + div {
+    margin-top: 32px;
+  }
+
+  &:hover {
+    box-shadow: inset 0 0 1em transparent,
+      0 0 0.4em ${props => props.theme.colors.primary};
+  }
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: ${props.theme.colors.red};
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: ${props.theme.colors.primary};
+      border-color: ${props.theme.colors.primary};
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: ${props.theme.colors.primary};
+    `}
+
+  input {
+    background: transparent;
+    flex: 1;
+    border: 0;
+    color: ${props => props.theme.colors.black};
+
+    &::placeholder {
+      color: ${props => props.theme.colors.grayLight};
+    }
+  }
+
+  svg:nth-of-type(1) {
+    cursor: pointer;
+  }
+
+  svg:nth-of-type(2) {
+    margin-left: 8px;
+  }
+`;
+
+export const IconContainer = styled.div`
+  background: transparent;
+  width: 34px;
+  height: 34px;
+  border-radius: 8px;
+
+  cursor: pointer;
+
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background: #eee;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-right: 4px;
+
+  span {
+    color: ${props => props.theme.colors.white} !important;
+    background: ${props => props.theme.colors.red};
+
+    &::before {
+      border-color: ${props => props.theme.colors.red} transparent;
+    }
+  }
+`;
