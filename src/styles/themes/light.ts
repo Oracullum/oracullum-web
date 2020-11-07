0cc5ee057a838b77eb346a68b7a1fbc1cdca349b
Bruno Futema <bruno.futema@outlook.com>
2020-11-06 22:25:10 -0300

    feat: :sparkles: Add light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,33 @@
+export default {
+  title: 'light',
+  colors: {
+    primaryLighter: '#FF953F',
+    primaryLight: '#FF7300',
+    primary: '#FD520F',
+
+    secondaryLighter: '#656565',
+    secondaryLight: '#414345',
+    secondary: '#232526',
+
+    primaryGradient: 'linear-gradient(90deg, #FF512F 0%, #F09819 100%);',
+    secondaryGradient: 'linear-gradient(90deg, #232526 0%, #414345 100%);',
+
+    white: '#FBFBFB',
+    black: '#232526',
+    greenLight: '#AFF579',
+    green: '#28DF99',
+    purple: '#8B56E9',
+    yellow: '#FFFF33',
+    blue: '#4B75E7',
+
+    grayLight: '#909090',
+    gray: '#808080',
+    grayDark: '#707070',
+
+    background: '#FBFBFB',
+    text: '#232526',
+
+    textInPrimary: '#FD520F',
+    textInSecondary: '#FBFBFB',
+  },
+};
