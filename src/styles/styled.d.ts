15b6b444d085a8508a85f9dbfe68c48f6c8e068b
Bruno Futema <bruno.futema@outlook.com>
2020-11-06 22:32:35 -0300

    feat: Add theme types for typescript

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,37 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    title: string;
+    colors: {
+      primaryLighter: string;
+      primaryLight: string;
+      primary: string;
+
+      secondaryLighter: string;
+      secondaryLight: string;
+      secondary: string;
+
+      primaryGradient: string;
+      secondaryGradient: string;
+
+      white: string;
+      black: string;
+      greenLight: string;
+      green: string;
+      purple: string;
+      yellow: string;
+      blue: string;
+
+      grayLight: string;
+      gray: string;
+      grayDark: string;
+
+      background: string;
+      text: string;
+
+      textInPrimary: string;
+      textInSecondary: string;
+    };
+  }
+}
