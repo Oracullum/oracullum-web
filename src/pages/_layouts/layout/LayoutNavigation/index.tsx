961df8c2ecffd94e12813f8f627f19833a180e9d
Bruno Futema <bruno.futema@outlook.com>
2020-11-07 23:14:37 -0300

    feat: :sparkles: Add layout control of application

---
+++ src/pages/_layouts/layout/LayoutNavigation/index.tsx
@@ -0,0 +1,115 @@
+import React, { useState, useCallback, useContext } from 'react';
+import { ThemeContext } from 'styled-components';
+
+import Dashboard from '../../../Dashboard';
+
+import oracullumLightImg from '../../../../assets/oracullum-light.svg';
+import oracullumDarkImg from '../../../../assets/oracullum-dark.svg';
+
+import {
+  Container,
+  AsideNavigationContainer,
+  AsideNavigationItem,
+  FaThLarge,
+  FaChartLine,
+  FaAlignLeft,
+  FaLayerGroup,
+  ContentWrapper,
+} from './styles';
+
+const LayoutNavigation: React.FC = () => {
+  const { title } = useContext(ThemeContext);
+
+  const [dashboardActive, setDashboardActive] = useState(true);
+  const [discoverActive, setDiscoverActive] = useState(false);
+  const [actionActive, setActionActive] = useState(false);
+  const [reportActive, setReportActive] = useState(false);
+
+  const handleDashboardActive = useCallback(() => {
+    setDashboardActive(true);
+    setDiscoverActive(false);
+    setActionActive(false);
+    setReportActive(false);
+  }, []);
+
+  const handleDiscoverActive = useCallback(() => {
+    setDashboardActive(false);
+    setDiscoverActive(true);
+    setActionActive(false);
+    setReportActive(false);
+  }, []);
+
+  const handleActionsActive = useCallback(() => {
+    setDashboardActive(false);
+    setDiscoverActive(false);
+    setActionActive(true);
+    setReportActive(false);
+  }, []);
+
+  const handleReportActive = useCallback(() => {
+    setDashboardActive(false);
+    setDiscoverActive(false);
+    setActionActive(false);
+    setReportActive(true);
+  }, []);
+
+  return (
+    <Container>
+      <AsideNavigationContainer>
+        <img
+          src={title === 'light' ? oracullumLightImg : oracullumDarkImg}
+          alt="Oracullum"
+        />
+
+        <AsideNavigationItem
+          active={dashboardActive}
+          onClick={handleDashboardActive}
+        >
+          <FaThLarge active={dashboardActive} />
+
+          <div />
+
+          <span>Dashboard</span>
+        </AsideNavigationItem>
+
+        <AsideNavigationItem
+          active={discoverActive}
+          onClick={handleDiscoverActive}
+        >
+          <FaChartLine active={discoverActive} />
+
+          <div />
+
+          <span>Descobrir</span>
+        </AsideNavigationItem>
+
+        <AsideNavigationItem
+          active={actionActive}
+          onClick={handleActionsActive}
+        >
+          <FaAlignLeft active={actionActive} />
+
+          <div />
+
+          <span>Todas Ações</span>
+        </AsideNavigationItem>
+
+        <AsideNavigationItem active={reportActive} onClick={handleReportActive}>
+          <FaLayerGroup active={reportActive} />
+
+          <div />
+
+          <span>Relatórios</span>
+        </AsideNavigationItem>
+      </AsideNavigationContainer>
+      <ContentWrapper>
+        {dashboardActive && <Dashboard />}
+        {discoverActive && <h1>Descobrir</h1>}
+        {actionActive && <h1>Todas Ações</h1>}
+        {reportActive && <h1>Relatórios</h1>}
+      </ContentWrapper>
+    </Container>
+  );
+};
+
+export default LayoutNavigation;
---
+++ src/pages/_layouts/layout/LayoutNavigation/styles.ts
@@ -0,0 +1,173 @@
+import styled, { css } from 'styled-components';
+import { lighten, shade } from 'polished';
+
+import { IconBaseProps } from 'react-icons';
+
+import {
+  FaThLarge as FaThLargeOriginal,
+  FaChartLine as FaChartLineOriginal,
+  FaAlignLeft as FaAlignLeftOriginal,
+  FaLayerGroup as FaLayerGroupOriginal,
+} from 'react-icons/fa';
+
+export const Container = styled.div`
+  display: flex;
+
+  height: 100%;
+`;
+
+export const AsideNavigationContainer = styled.div`
+  padding: 16px;
+
+  img {
+    width: 6rem;
+    margin-bottom: 22px;
+  }
+
+  div + div {
+    margin-top: 1rem;
+  }
+`;
+
+interface AsideNavigationItemProps {
+  active: boolean;
+}
+
+export const AsideNavigationItem = styled.div<AsideNavigationItemProps>`
+  user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+
+  background: ${props =>
+    props.active && props.theme.title === 'light'
+      ? props.theme.colors.white
+      : props.active && props.theme.title === 'dark'
+      ? props.theme.colors.black
+      : 'transparent'};
+
+  border-radius: 16px;
+  padding: 4px;
+
+  width: 6rem;
+  height: 5rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+
+  position: relative;
+
+  ${props =>
+    props.active &&
+    css`
+      &:after {
+        content: '';
+        position: absolute;
+
+        background-color: transparent;
+        top: -50px;
+        right: -16px;
+        height: 50px;
+        width: 16px;
+        border-bottom-right-radius: 25px;
+        box-shadow: 0 25px 0 0
+          ${props.theme.title === 'light'
+            ? props.theme.colors.white
+            : props.theme.colors.black};
+      }
+
+      div {
+        background: ${props.theme.title === 'light'
+          ? props.theme.colors.white
+          : props.theme.colors.black};
+        width: 64px;
+        height: 80px;
+        right: -16px;
+        position: absolute;
+
+        z-index: -1;
+      }
+
+      &:before {
+        content: '';
+        position: absolute;
+
+        background-color: transparent;
+        bottom: -50px;
+        right: -16px;
+        height: 50px;
+        width: 16px;
+        border-top-right-radius: 25px;
+        box-shadow: 0 -25px 0 0 ${props.theme.title === 'light' ? props.theme.colors.white : props.theme.colors.black};
+      }
+    `}
+
+  &:hover {
+    color: ${props => !props.active && lighten(0.1, props.theme.colors.white)};
+    background: ${props =>
+      !props.active && shade(0.2, props.theme.colors.primary)};
+
+    svg {
+      color: ${props =>
+        !props.active && lighten(0.1, props.theme.colors.white)};
+    }
+  }
+
+  svg {
+    margin-bottom: 0.6rem;
+  }
+
+  span {
+    color: ${props => props.active && props.theme.colors.primary};
+    font-weight: bold;
+  }
+`;
+
+interface IconProps extends IconBaseProps {
+  active: boolean;
+}
+
+export const FaThLarge = styled(FaThLargeOriginal).attrs({
+  size: 28,
+})<IconProps>`
+  color: ${props =>
+    props.active ? props.theme.colors.primary : props.theme.colors.black};
+`;
+
+export const FaChartLine = styled(FaChartLineOriginal).attrs({
+  size: 28,
+})<IconProps>`
+  color: ${props =>
+    props.active ? props.theme.colors.primary : props.theme.colors.black};
+`;
+
+export const FaAlignLeft = styled(FaAlignLeftOriginal).attrs({
+  size: 28,
+})<IconProps>`
+  color: ${props =>
+    props.active ? props.theme.colors.primary : props.theme.colors.black};
+`;
+
+export const FaLayerGroup = styled(FaLayerGroupOriginal).attrs({
+  size: 28,
+})<IconProps>`
+  color: ${props =>
+    props.active ? props.theme.colors.primary : props.theme.colors.black};
+`;
+
+export const ContentWrapper = styled.div`
+  color: ${props => props.theme.colors.text};
+  background: ${props =>
+    props.theme.title === 'light'
+      ? props.theme.colors.white
+      : props.theme.colors.black};
+
+  width: 100%;
+  height: 100%;
+
+  padding: 32px;
+`;
