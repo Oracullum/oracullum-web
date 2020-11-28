e9960d85102278d087a038ddeac65bdb456edce0
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-28 17:25:12 -0300

    fix: removing action and using eslint

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,28 +1,22 @@
 import React, { useCallback, useRef } from 'react';
-import { ExternalLogins, Presentation } from '../SignIn/styles';
-import { Container } from './styles';
 import { Form } from '@unform/web';
+import { Link as ReactLink } from 'react-router-dom';
+import { FormHandles } from '@unform/core';
+import { ExternalLogins, Presentation } from '../SignIn/styles';
+import { Container, AccessContainer } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import heroImg from '../../assets/hero.svg';
 
-import { Link as ReactLink } from 'react-router-dom';
-import { FormHandles } from '@unform/core';
-
-import {
-  AccessContainer
-} from './styles';
-
 const SignUp: React.FC = () => {
-  
   const formRef = useRef<FormHandles>(null);
 
   const handleSubmit = useCallback(async (data: object) => {
     console.log(data);
   }, []);
-  
+
   return (
-    <Container > 
+    <Container>
       <Presentation backgroundImg={heroImg}>
         <ReactLink to="/">
           <img src={logoImg} alt="Oracullum" />
@@ -34,15 +28,13 @@ const SignUp: React.FC = () => {
       </Presentation>
       <Form ref={formRef} onSubmit={handleSubmit}>
         <AccessContainer>
-          <span>Crie sua conta <strong>Oracullum:</strong></span>
-
-          
-
+          <span>
+            Crie sua conta <strong>Oracullum:</strong>
+          </span>
         </AccessContainer>
-
       </Form>
-    </Container >
-    );
+    </Container>
+  );
 };
 
 export default SignUp;
