ec498badfda1edde2d042fabecbaa6ae5e74e4fa
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-28 23:22:05 -0300

    feat: developing the screen Exchange

--- src/pages/Exchanges/styles.ts
+++ src/pages/Exchanges/styles.ts
@@ -1,3 +1,40 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  > h1 {
+    margin-bottom: 64px;
+
+    position: relative;
+
+    width: 185px;
+
+    &:after {
+      content: '';
+
+      position: absolute;
+      bottom: -6px;
+      left: 0;
+
+      background: ${props => props.theme.colors.primaryGradient};
+      width: 100%;
+      height: 2px;
+    }
+  }
+`;
+
+export const Tables = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  > div + div {
+    margin-left: 120px;
+  }
+`;
+
+export const Items = styled.div``;
