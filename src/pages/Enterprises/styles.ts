74a070d808a032e7fd096015c6c4c81956bf766f
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-29 20:36:27 -0300

    feat: developing the page enterprise

---
+++ src/pages/Enterprises/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  > h1 {
+    margin-bottom: 64px;
+
+    position: relative;
+
+    width: 135px;
+
+    &:after {
+      content: '';
+
+      position: absolute;
+      bottom: -6px;
+      left: 0;
+
+      background: ${props => props.theme.colors.primaryGradient};
+      width: 100%;
+      height: 2px;
+    }
+  }
+`;
+
+export const Items = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(380px, 1fr));
+  column-gap: 24px;
+  row-gap: 36px;
+`;
