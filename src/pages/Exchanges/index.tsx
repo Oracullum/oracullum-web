fecca98cc345b71ab4943706522b3fec2216e999
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-28 23:21:54 -0300

    feat: developing the screen Exchange

--- src/pages/Exchanges/index.tsx
+++ src/pages/Exchanges/index.tsx
@@ -1,14 +1,22 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from 'styled-components';
+import React from 'react';
+import Accordion from '../../components/Accordion';
+// import { ThemeContext } from 'styled-components';
 
-import { Container } from './styles';
+import { Container, Items } from './styles';
 
 const Exchanges: React.FC = () => {
-  const { colors } = useContext(ThemeContext);
+  // const { colors } = useContext(ThemeContext);
 
   return (
     <Container>
       <h1>Minhas ações</h1>
+
+      <Items>
+        <Accordion />
+        <Accordion />
+        {/* <Accordion /> */}
+        {/* <Accordion /> */}
+      </Items>
     </Container>
   );
 };
