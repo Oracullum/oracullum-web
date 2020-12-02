7af6b6997a14e6f0a87eec724def83c1b3301044
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-12-01 23:29:23 -0300

    feat: creating the body of table Profile

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import Navbar from '../../components/Navbar';
+import Card from '../../components/Card';
+import Exchange from '../../components/Exchange';
+
+import { Container, Header, Tables } from './styles';
+
+const Profile: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <h1>Descobrir</h1>
+
+        <Navbar />
+      </Header>
+
+      <Tables>
+        <Card title="Ações" width={20}>
+          <Exchange
+            actionName="POSI4"
+            companyName="Positivo Info."
+            initialValue={380.0}
+            stopValue={382.2}
+          />
+
+          <Exchange
+            actionName="POSI4"
+            companyName="Positivo Info."
+            initialValue={380.0}
+            stopValue={382.2}
+          />
+
+          <Exchange
+            actionName="POSI4"
+            companyName="Positivo Info."
+            initialValue={380.0}
+            stopValue={382.2}
+          />
+
+          <Exchange
+            actionName="POSI4"
+            companyName="Positivo Info."
+            initialValue={380.0}
+            stopValue={382.2}
+          />
+
+          <Exchange
+            actionName="POSI4"
+            companyName="Positivo Info."
+            initialValue={380.0}
+            stopValue={382.2}
+          />
+        </Card>
+
+        <Card title="Sugestões" width={80} />
+      </Tables>
+    </Container>
+  );
+};
+
+export default Profile;
