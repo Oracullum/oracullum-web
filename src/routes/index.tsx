fe0c65ae0e2c0267d863258f74a140c108038398
Bruno Futema <bruno.futema@outlook.com>
2020-12-06 14:30:38 -0300

    feat: :sparkles: Add private handler routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Router, Switch } from 'react-router-dom';
+import Route from './Route';
 
 import Landing from '../pages/Landing';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
+import history from '../services/history';
+
 import LayoutNavigation from '../pages/_layouts/layout/LayoutNavigation';
 
-const Routes: React.FC = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route path="/login" component={SignIn} />
-      <Route path="/register" component={SignUp} />
-      <Route path="/dashboard" component={LayoutNavigation} />
-      <Route path="/profile" component={LayoutNavigation} />
-    </Switch>
-  </BrowserRouter>
-);
+const Routes: React.FC = () => {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/login" component={SignIn} />
+        <Route path="/register" component={SignUp} />
+
+        <Route path="/dashboard" component={LayoutNavigation} isPrivate />
+      </Switch>
+    </Router>
+  );
+};
 
 export default Routes;
