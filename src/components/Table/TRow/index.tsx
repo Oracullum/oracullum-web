5ec3e727dccd6787e04e54e3bd6c2bdf64de221f
Bruno Futema <bruno.futema@outlook.com>
2020-12-07 23:17:23 -0300

    feat: :sparkles: Add TRow component

---
+++ src/components/Table/TRow/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { TableColumn } from '../THead';
+import TCell from '../TCell';
+
+import { Tr } from './styles';
+
+interface TRowProps {
+  columns: TableColumn[];
+  item: any;
+}
+
+const TRow: React.FC<TRowProps> = ({ columns, item }) => {
+  return (
+    <Tr key={item.id} isOperationBuy={item.type}>
+      {columns.map((column: TableColumn) => (
+        <TCell
+          key={column.property}
+          column={column}
+          item={item}
+          isOperationBuy={item.type}
+        />
+      ))}
+    </Tr>
+  );
+};
+
+export default TRow;
