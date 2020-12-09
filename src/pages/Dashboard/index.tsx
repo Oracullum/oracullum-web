c3100633779560496aefa7edd61a834e2a791221
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-12-09 18:16:26 -0300

    feat: adding message "Em Produção" into card

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -53,7 +53,11 @@ const Dashboard: React.FC = () => {
           />
         </Card>
 
-        <Card title="Sugestões" width={80} />
+        <Card title="Sugestões" width={80}>
+          <div className="card-production">
+            <span>EM PRODUÇÃO</span>
+          </div>
+        </Card>
       </Cards>
     </Container>
   );
